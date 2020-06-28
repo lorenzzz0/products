e6561e5091f5ffea47f3ec68ee2713c52a9b80d0
lorenzzz0 <lorenzzz0>
2020-06-27 19:17:14 -0500

    registrar applicación y creación del modelo

--- common/products/models.py
+++ common/products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Product( models.Model ):
+    dbid            = models.CharField( max_length= 100 )
+    name            = models.CharField( max_length= 100 )
+    value           = models.FloatField()
+    discount_value  = models.FloatField()
+    stock           = models.IntegerField()
